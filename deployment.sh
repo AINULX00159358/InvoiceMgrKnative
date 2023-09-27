7012af2930b330c130fe25a2315b018611d581c9
x00159358 <x00159358@mytudublin.ie>
2023-09-27 04:13:39 +0100

    added tag in deployment

--- deployment.sh
+++ deployment.sh
@@ -1,11 +1,11 @@
-kn service create auditwriter --env STARTUP=auditwriter --image x00159358/invoicemgrimg:latest
+kn service create auditwriter --env STARTUP=auditwriter --image x00159358/invoicemgrimg --tag @latest
 sleep 5
-kn service create invoicegenerator --env STARTUP=invoicegenerator --image x00159358/invoicemgrimg:latest
+kn service create invoicegenerator --env STARTUP=invoicegenerator --image x00159358/invoicemgrimg --tag @latest
 sleep 5
-kn service create invoiceregister --env STARTUP=invoiceregister --image x00159358/invoicemgrimg:latest
+kn service create invoiceregister --env STARTUP=invoiceregister --image x00159358/invoicemgrimg --tag @latest
 sleep 5 
-kn service create invoicepayment --env STARTUP=invoicepayment --image x00159358/invoicemgrimg:latest
+kn service create invoicepayment --env STARTUP=invoicepayment --image x00159358/invoicemgrimg --tag @latest
 sleep 5
-kn service create invoicevalidation --env STARTUP=invoicevalidation --image x00159358/invoicemgrimg:latest
+kn service create invoicevalidation --env STARTUP=invoicevalidation --image x00159358/invoicemgrimg --tag @latest
 sleep 5
 kn service list
