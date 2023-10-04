b034ac2948558c77d7217c1bc22ab397467d3516
AINULX00159358 <60287526+AINULX00159358@users.noreply.github.com>
2023-10-04 21:05:26 +0100

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,11 +1,11 @@
-kn trigger create invoicegenerator-trigger --broker example-broker --filter type=InvoiceGenRequest --sink invoicegenerator
+kn trigger create invoicegenerator-trigger --broker invoicemgr-event-broker --filter type=InvoiceGenRequest --sink invoicegenerator
 sleep 5
-kn trigger create invoiceregister-trigger --broker example-broker --filter type=invoice.NEW --sink invoiceregister
+kn trigger create invoiceregister-trigger --broker invoicemgr-event-broker --filter type=invoice.NEW --sink invoiceregister
 sleep 5
-kn trigger create invoicepayment-trigger --broker example-broker --filter type=invoice.PENDING --sink invoicepayment
+kn trigger create invoicepayment-trigger --broker invoicemgr-event-broker --filter type=invoice.PENDING --sink invoicepayment
 sleep 5
-kn trigger create invoicevalidation-trigger --broker example-broker --filter type=invoice.PAID --sink invoicevalidation
+kn trigger create invoicevalidation-trigger --broker invoicemgr-event-broker --filter type=invoice.PAID --sink invoicevalidation
 sleep 5
-kn trigger create auditwriter-trigger --broker example-broker --filter type=invoice.CLOSED --sink auditwriter
+kn trigger create auditwriter-trigger --broker invoicemgr-event-broker --filter type=invoice.CLOSED --sink auditwriter
 sleep 5
 kn trigger list
