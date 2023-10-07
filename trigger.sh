3a1e2d1bdb617f918520164f02b5da5736f84798
AINULX00159358 <60287526+AINULX00159358@users.noreply.github.com>
2023-10-07 19:32:11 +0100

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,11 +1,11 @@
-kn trigger create invoicegenerator-trigger --broker invoicemgr-event-broker --filter type=InvoiceGenRequest --sink invoicegenerator
+kn trigger create invoicegenerator-trigger --broker invoicemgr-event-broker --filter type=InvoiceGenRequest --sink ksvc:invoicegenerator
 sleep 5
-kn trigger create invoiceregister-trigger --broker invoicemgr-event-broker --filter type=invoice.NEW --sink invoiceregister
+kn trigger create invoiceregister-trigger --broker invoicemgr-event-broker --filter type=invoice.NEW --sink ksvc:invoiceregister
 sleep 5
-kn trigger create invoicepayment-trigger --broker invoicemgr-event-broker --filter type=invoice.PENDING --sink invoicepayment
+kn trigger create invoicepayment-trigger --broker invoicemgr-event-broker --filter type=invoice.PENDING --sink ksvc:invoicepayment
 sleep 5
-kn trigger create invoicevalidation-trigger --broker invoicemgr-event-broker --filter type=invoice.PAID --sink invoicevalidation
+kn trigger create invoicevalidation-trigger --broker invoicemgr-event-broker --filter type=invoice.PAID --sink ksvc:invoicevalidation
 sleep 5
-kn trigger create auditwriter-trigger --broker invoicemgr-event-broker --filter type=invoice.CLOSED --sink auditwriter
+kn trigger create auditwriter-trigger --broker invoicemgr-event-broker --filter type=invoice.CLOSED --sink ksvc:auditwriter
 sleep 5
 kn trigger list
