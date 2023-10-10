0328d916e2de2f2b86f2ec971c10f7522ecd7444
AINULX00159358 <60287526+AINULX00159358@users.noreply.github.com>
2023-10-10 12:41:30 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -2,7 +2,7 @@ export SCALE_TARGET=120
 export SCALE_UTIL_PERC=80
 echo "Application will scale once TPS is increased over 120 (i.e scale_target * (scale_utilization/100))"
 echo "Deploying Services"
-kn service create auditwriter --env STARTUP=auditwriter --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --scale-metric=rps --scale-target=${SCALE_TARGET} --scale-utilization=${SCALE_UTIL_PERC} 
+kn service create invoiceaudit --env STARTUP=auditwriter --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --scale-metric=rps --scale-target=${SCALE_TARGET} --scale-utilization=${SCALE_UTIL_PERC} 
 sleep 5
 kn service create invoicegenerator --env STARTUP=invoicegenerator --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --scale-metric=rps --scale-target=${SCALE_TARGET} --scale-utilization=${SCALE_UTIL_PERC}
 sleep 5
