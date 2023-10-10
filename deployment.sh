514412872883de2d0dc9945cb304881e89995b01
AINULX00159358 <60287526+AINULX00159358@users.noreply.github.com>
2023-10-10 11:59:49 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,11 +1,20 @@
-kn service create auditwriter --env STARTUP=auditwriter --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --request memory=256Mi,cpu=250m --limit cpu=250m,memory=256Mi --scale-metric=rps
+echo "Deleteing Services if Exist"
+kn service delete auditwriter || true
+kn service delete invoicegenerator || true
+kn service delete invoiceregister || true
+kn service delete invoicepayment || true
+kn service delete invoicevalidation || true
 sleep 5
-kn service create invoicegenerator --env STARTUP=invoicegenerator --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --request memory=256Mi,cpu=250m --limit cpu=250m,memory=256Mi --scale-metric=rps
+echo "Deploying Services"
+kn service create auditwriter --env STARTUP=auditwriter --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --scale-metric=rps
 sleep 5
-kn service create invoiceregister --env STARTUP=invoiceregister --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --request memory=256Mi,cpu=250m --limit cpu=250m,memory=256Mi --scale-metric=rps
+kn service create invoicegenerator --env STARTUP=invoicegenerator --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --scale-metric=rps
+sleep 5
+kn service create invoiceregister --env STARTUP=invoiceregister --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --scale-metric=rps
 sleep 5 
-kn service create invoicepayment --env STARTUP=invoicepayment --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --request memory=256Mi,cpu=250m --limit cpu=250m,memory=256Mi --scale-metric=rps
+kn service create invoicepayment --env STARTUP=invoicepayment --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --scale-metric=rps
 sleep 5
-kn service create invoicevalidation --env STARTUP=invoicevalidation --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --request memory=256Mi,cpu=250m --limit cpu=250m,memory=256Mi --scale-metric=rps
+kn service create invoicevalidation --env STARTUP=invoicevalidation --image x00159358/invoicemgrimg:latest --label app.solution=invoicemgr --scale-metric=rps
 sleep 5
 kn service list
+
