0a1705577af9621a74d0b7245447e8e03adaee57
AINULX00159358 <60287526+AINULX00159358@users.noreply.github.com>
2023-10-14 06:20:23 +0100

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,13 +1,14 @@
 export SCALE_TARGET=150
 export SCALE_UTIL_PERC=95
-export REQUEST=cpu=1000m,memory=1024Mi
+export REQUEST=cpu=500m,memory=512Mi
+export LIMIT=cpu=1000m,memory=1024Mi
 
 kn_service_create() {
     echo "DEPLOYING KNATIVE SERVICE $1"
-    kn service create $1 --env STARTUP=$1 --image x00159358/invoicemgrimg:latest --pull-policy=IfNotPresent --label app.solution=invoicemgr --scale-metric=rps --scale-target=${SCALE_TARGET} --scale-utilization=${SCALE_UTIL_PERC}
+    kn service create $1 --env STARTUP=$1 --image x00159358/invoicemgrimg:latest --pull-policy=IfNotPresent --label app.solution=invoicemgr --request ${REQUEST} --limit ${LIMIT} --scale-metric=rps --scale-target=${SCALE_TARGET} --scale-utilization=${SCALE_UTIL_PERC}
 }
 
-echo "Resources Requrested is ${REQUEST}"
+echo "Resources Requrested is ${REQUEST} with limit of ${LIMIT}"
 echo "Target RPS is ${SCALE_TARGET} and Utilization is ${SCALE_UTIL_PERC}"
 echo "Application will scale once TPS is increased over $(($SCALE_TARGET * $SCALE_UTIL_PERC/100)) (i.e scale_target * (scale_utilization/100))"
 
